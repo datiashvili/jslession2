cf313ebd1d4d311a80ac149806731b50087883fe
datiashvilinika <nikushadtia24@gmail.com>
2025-03-16 14:17:08 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+console.log('hello')
+const num1 = 100
+let num2 = 10
+console.log(num1)
+console.log(num2)
+num2 = 80
+console.log(num2)
+const x1 = 10 + 20
+const x2 = 10 - 20
+const x3 = 10 * 20
+const x4 = 10 / 20
+console.log(x1)
+console.log(x2)
+console.log(x3)
+console.log(x4)
+const name1 = 'mike'
+let name2 = 'nika'
+console.log(name1)
+console.log(name2)
+const MyName = 'nika'
+const mylastname = 'datiashvili'
+console.log(mylastname)
+console.log(MyName)
+name2 = 'luka'
+console.log(name2)
+console.log(name1.length)
+const hh1 = document.querySelector('.hh1')
+console.log(hh1)
+const pp1 = document.querySelector('.pp1')
+console.log(pp1)
+hh1.style.color = 'red'
+hh1.style.border = '2px solid black'
+hh1.style.borderRadius = '20px'
+pp1.style.color = 'red'
+pp1.style.border = '3px solid black'
+pp1.style.borderRadius = '15px'
+pp1.style.backgroundColor = 'yellow'
+pp1.style.fontSize = '60px'
+hh1.textContent = 'suiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii'
+pp1.textContent = 'no its not that hard'
+const dd1 = document.querySelector('.div1')
+console.log(dd1)
+dd1.style.color = 'yellow'
+dd1.style.backgroundColor = 'black'
+dd1.style.fontSize = '40px'
+dd1.textContent = 'es aris pirveli div elementi'
+const btn = document.querySelector('.btn')
+console.log(btn)
+btn.addEventListener('click', () => {
+    console.log('hello')
+})
+hh1.addEventListener('click', () => {
+    console.log('Cristiano Ronaldo')
+})
+pp1.addEventListener('click', () => {
+    console.log('jss!')
+})
